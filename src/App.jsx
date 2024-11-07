45f060c3206e7b4ea896ef501760e92bdc49cb59
monicajorgedev <monicajorge.dev@gmail.com>
2024-11-07 12:46:03 +0100

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import useFecthCharacters from './hooks/useFecthCharacters';
 import './App.css';
 
 function App() {
   const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
   const urlRick = 'https://rickandmortyapi.com/api/character/1';
   
+  const FetchPokemon = useFecthCharacters(urlPokemon)
+  const FetchRick = useFecthCharacters(urlRick)
+
   return (
     <>
+      <h2>Personaje Pokemon</h2>
+      <p>{FetchPokemon.character}</p>
+      <img src={FetchPokemon.imgUrl}  />
+
+      <h2>Personaje Rick and Morty</h2>
+      <p>{FetchRick.character}</p>
+      <img src={FetchRick.imgUrl}  />
     </>
   );
 }
